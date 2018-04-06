985d64c8c67db6bc8569c7f4411849e6b1eb40eb
Mohammed Arfan <35273346+mdarfan@users.noreply.github.com>
2018-04-06 22:41:20 +0530

    Add files via upload

---
+++ thinkspeak_fetch_temp/iiotdatathingspeak.ino
@@ -0,0 +1,126 @@
+/*
+ *  HTTP over TLS (HTTPS) example sketch
+ *
+ *  This example demonstrates how to use
+ *  WiFiClientSecure class to access HTTPS API.
+ *  We fetch and display the status of
+ *  esp8266/Arduino project continuous integration
+ *  build.
+ *
+ *  Limitations:
+ *    only RSA certificates
+ *    no support of Perfect Forward Secrecy (PFS)
+ *    TLSv1.2 is supported since version 2.4.0-rc1
+ *
+ *  Created by Ivan Grokhotkov, 2015.
+ *  This example is in public domain.
+ */
+
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+
+const char* ssid = "MSB";
+const char* password = "bautomate";
+
+const char* host = "api.thingspeak.com";
+const int httpsPort = 443;
+
+// Use web browser to view and copy
+// SHA1 fingerprint of the certificate
+const char* fingerprint = "d99371c78e5ab128d3bbcd70857983beb74a9ca5";
+
+int find_text(String needle, String haystack,int from) {
+  int foundpos = -1;
+  if (haystack.length()<needle.length())
+    return foundpos;
+  for (int i = from; (i < haystack.length() - needle.length()); i++) {
+    if (haystack.substring(i,needle.length()+i) == needle) {
+      foundpos = i;
+      return foundpos;
+    }
+  }
+  return foundpos;
+}
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("connecting to ");
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Use WiFiClientSecure class to create TLS connection
+  WiFiClientSecure client;
+  Serial.print("connecting to ");
+  Serial.println(host);
+  if (!client.connect(host, httpsPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+
+  if (client.verify(fingerprint, host)) {
+    Serial.println("certificate matches");
+  } else {
+    Serial.println("certificate doesn't match");
+  }
+
+  String url = "https://api.thingspeak.com/apps/thinghttp/send_request?api_key=1UCXJB8ULCVE81B1";
+  Serial.print("requesting URL: ");
+  Serial.println(url);
+
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "User-Agent: BuildFailureDetectorESP8266\r\n" +
+               "Connection: close\r\n\r\n");
+
+  Serial.println("request sent");
+  while (client.connected()) {
+    String line = client.readStringUntil('\n');
+    if (line == "\r") {
+      Serial.println("headers received");
+      break;
+    }
+  }
+  String line = client.readStringUntil('\n');
+  if (line.startsWith("{\"state\":\"success\"")) {
+    Serial.println("esp8266/Arduino CI successfull!");
+  } else {
+    Serial.println("esp8266/Arduino CI has failed");
+  }
+  Serial.println("reply was:");
+  Serial.println("==========");
+  Serial.println(line);
+  Serial.println("==========");
+  Serial.println("closing connection");
+  int start_loc =find_text(":",line,0);
+  int end_loc =find_text("%",line,0);
+  Serial.println(start_loc);
+  Serial.println(end_loc);
+  Serial.println(line[start_loc+2]);
+  Serial.println(line[start_loc+3]);
+  int rain =int(line[start_loc+2]);
+  Serial.print("rain = ");
+  Serial.println(rain);
+  if(rain>51)
+  {
+    Serial.println("its going to rain");
+  }
+  else
+  {
+    Serial.println("water the plant");
+  }
+  
+}
+
+void loop() {
+}
